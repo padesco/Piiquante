a7cc57946bb5fa838e028883d3e0010b0c649f89
padesco <pa.desco@live.com>
2022-09-27 10:24:05 +0200

    last commit

--- middleware/emailValidator.js
+++ middleware/emailValidator.js
@@ -1,12 +1,12 @@
 // importation du package "email-validator" pour s'assurer que le champ soit renseigné avec un email
-const emailValidator = require('email-validator');
+const emailValidator = require('validator');
 
 // on exporte le module et on inclu une condition 
 module.exports = (req, res, next) => {
     // si le password répond aux critères on continu
-    if(emailValidator.validate(req.body.email)){
+    if(emailValidator.isEmail(req.body.email)){
         next();
     } else { // sinon on bloque et message d'erreur
-        return res.status(400).json({error})
+        return res.status(400).json({error: "email not valid"})
     }
 };
